edfdcbcc3d3f56f4e864956bcbee20cfa3bc3467
Tanziha-Arshad <tanzihaarshad29@gmail.com>
2024-07-05 16:40:48 +0500

    Add files via upload

---
+++ helloworld-to-input.py
@@ -0,0 +1,16 @@
+print("HELLO WORLD!")
+
+
+vari=input("write your rollno: ")
+print(vari)
+
+x=0
+#initially x is zero
+x=input("write value of x:")
+print(x)                   #when u take input like this variable  becomes  string type 
+                    
+x=int(x)                     #make it into int type
+if x>0:
+   print("my careeer")
+
+

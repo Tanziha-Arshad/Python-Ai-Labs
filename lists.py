d27057312f49668266ab688ad01cd03ef58cf1e5
Tanziha-Arshad <tanzihaarshad29@gmail.com>
2024-07-08 03:05:13 +0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+integer=[10,20,30]
+float=[3.80,3.81,3.86,3.85]
+string=['red','blue','green']
+mixed=[32,6.7,'navy blue']
+
+print(integer)
+print(float)
+print(string)
+print(mixed)
+
+
+#SLICING WITHIN THE LISTS
+LIST=[10,20,30,40,50,60,70,80,90,100]
+print(LIST[2:5])
+print(LIST[-9:-1])
+

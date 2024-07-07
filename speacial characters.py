4569c092ef2e006c0c8dd11f15aa9d47a00a80b2
Tanziha-Arshad <tanzihaarshad29@gmail.com>
2024-07-08 02:41:51 +0500

    Add files via upload

---
+++ speacial characters.py
@@ -0,0 +1,11 @@
+#speacial characters using backslash
+s_ch="backslash= \ "
+print(s_ch)
+s_ch="single_quote= '\n "
+print(s_ch)
+s_ch="double_qoute= \"\n "
+print(s_ch)
+s_ch="new_line=  \n new line"
+print(s_ch)
+s_ch="tab_space= \t tabbed space "
+print(s_ch)

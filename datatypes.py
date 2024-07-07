41ef8b37ae9673a58a85222e563591e067a68a47
Tanziha-Arshad <tanzihaarshad29@gmail.com>
2024-07-08 02:12:31 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+age=23
+name="Tanziha"
+cgpa=3.80
+vacation=False
+dog=None
+z=complex(1,2)
+print(type(age))
+print(type(name))
+print(type(cgpa))
+print(type(vacation))
+print(type(dog))
+print(type(z))
